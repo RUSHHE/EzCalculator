36f7c67da02d5ecdadcc376a026a2638352d0a27
RUSHHE <rushhe@zjxu.edu.cn>
2024-05-10 10:40:24 +0800

    删除了未被使用的接口回调

--- app/src/main/java/com/example/ezcalculator/CalculateButtonSpanCallback.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ezcalculator
-
-interface CalculateButtonSpanCallback {
-    fun request()
-}
--- app/src/main/java/com/example/ezcalculator/CalculateButtonSpanListenerUser.kt
+++
@@ -1,8 +0,0 @@
-package com.example.ezcalculator
-
-class CalculateButtonSpanListenerUser {
-    lateinit var requestSpanListener: CalculateButtonSpanCallback
-    fun useRequestListener() {
-        requestSpanListener.request()
-    }
-}
